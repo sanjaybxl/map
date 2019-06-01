056f7ed020e464dba64300d3fea19b235e5a905c
sanjaybxl <iamsanjayb@gmail.com>
2019-06-01 22:48:42 +0530

    style-fix

--- src/app/forecasted-weather/forecasted-weather.component.ts
+++ src/app/forecasted-weather/forecasted-weather.component.ts
@@ -10,15 +10,18 @@ export class ForecastedWeatherComponent implements OnInit {
 
   constructor() { }
   dropDownItems: DropdownItem[] = [
-    { text: 'Alarms', colors: ['rgb(120, 0, 12)', 'rgb(244, 67, 54)', 'rgb(233, 30, 99)'] },
+    { text: 'Alarms', colors: ['rgb(120, 0, 12)', 'rgb(244, 67, 54)', 'rgb(233, 30, 99)', 'black'] },
     { text: 'Network', colors: ['red', 'green'] },
     { text: 'Orders', colors: ['red', 'green'] },
     { text: 'Storm Assist', colors: ['red', 'green'] }
   ];
-  selectedItem: DropdownItem =
+  selectedItem: DropdownItem = 
     { text: 'Alarms', colors: ['rgb(120, 0, 12)', 'rgb(244, 67, 54)', 'rgb(233, 30, 99)'] }
 
   ngOnInit() {
   }
 
+  onSelected(event:any) {
+    this.selectedItem = event;
+  }
 }
